a3c32512cc0f2bed105f83e234f6ad7e730ba138
alexBob <bboybrelock@gmail.com>
2021-01-18 15:31:54 +0200

    fix eslint bugs

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -4,44 +4,38 @@ import welcomeToGame from '../src/cli.js';
 
 welcomeToGame();
 
-
-let globName = ""
-function nameSet() {
-  const name = readlineSync.question('May I have your name? ');
-  console.log('Hello', name, '!');
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  
-  globName = name
-  generateRound()
-}
-
-let numAnswer = 0
-function checkOfNumber(question, correctAnswer) {
-  console.log("Question:", question);
-  const  answer = readlineSync.question('Your answer: ');
-  numAnswer += 1
-  if (numAnswer < 3) {
-    if (answer === correctAnswer) {
-      console.log("Correct!");
-      generateRound()
-    } else if (answer !== correctAnswer) {
-      console.log("Let's try again,",  globName, '!');
-      return
-    }
-    
-  } else if (numAnswer = 3) {
-    console.log('Congratulations,', globName)
-  }
-}
+let numAnswer = 0;
+let globName = '';
 
 const isEvenNumber = (num) => (num % 2 === 0);
 const [min, max] = [1, 10];
 
 function generateRound() {
-  let question = Math.floor(Math.random() * (max - min + 1)) + min
-  let correctAnswer = isEvenNumber(question) ? 'yes' : 'no';
+  const question = Math.floor(Math.random() * (max - min + 1)) + min;
+  const correctAnswer = isEvenNumber(question) ? 'yes' : 'no';
 
-  checkOfNumber(question, correctAnswer);
+  console.log('Question:', question);
+  const answer = readlineSync.question('Your answer: ');
+  numAnswer += 1;
+  if (numAnswer < 3) {
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+      generateRound();
+    } else if (answer !== correctAnswer) {
+      console.log("Let's try again,", globName, '!');
+    }
+  } else if (numAnswer === 3) {
+    console.log('Congratulations,', globName);
+  }
 }
 
-nameSet()
+function nameSet() {
+  const name = readlineSync.question('May I have your name? ');
+  console.log('Hello', name, '!');
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  globName = name;
+  generateRound();
+}
+
+nameSet();
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,5 @@
-import readlineSync from 'readline-sync';
-
 function welcomeToGame() {
   console.log('Welcome to the Brain Games!');
 }
 
-export default welcomeToGame
+export default welcomeToGame;
