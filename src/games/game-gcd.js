b57a5b4e33e5c830acb1cdc7901954b9309d9574
alexBob <bboybrelock@gmail.com>
2021-02-02 15:11:53 +0200

    minor fix

--- src/games/game-gcd.js
+++ src/games/game-gcd.js
@@ -5,10 +5,10 @@ const description = 'Find the greatest common divisor of given numbers.';
 
 const findGreatestDivisor = (a, b) => (b === 0 ? a : findGreatestDivisor(b, a % b));
 const generateQuestionAndAnswer = () => {
-  const firstNumber = generateRandomNum(1, 110);
-  const secondNumber = generateRandomNum(1, 60);
-  const question = `${firstNumber} ${secondNumber}`;
-  const answer = findGreatestDivisor(firstNumber, secondNumber).toString();
+  const number1 = generateRandomNum(1, 10);
+  const number2 = generateRandomNum(10, 20);
+  const answer = findGreatestDivisor(number1, number2).toString();
+  const question = `${number1} ${number2}`;
   return [question, answer];
 };
 
